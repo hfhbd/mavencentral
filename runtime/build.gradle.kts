eb03c9f627327cc1b997c5e44c2caa9d4d66469b
hfhbd <22521688+hfhbd@users.noreply.github.com>
2024-11-02 11:53:51 +0100

    WIP

--- runtime/build.gradle.kts
+++ runtime/build.gradle.kts
@@ -1,21 +1,32 @@
 plugins {
-    id("kotlinSetup")
+    id("java-library")
 }
 
-kotlin.jvmToolchain(11)
-
-dependencies {
-    api(projects.centralApi)
-    api(libs.ktor.client.java)
-    api(libs.ktor.client.logging)
-    api(libs.ktor.client.content.negotiation)
-    api(libs.ktor.serialization.kotlinx.json)
-}
-
-dependencies {
-    mavenCentralWorker(libs.mavencentral.runtime) {
-        version {
-            strictly(libs.versions.mavencentral.get())
+val s = configurations.dependencyScope("s")
+val d = configurations.resolvable("fooo") {
+    extendsFrom(s.get())
+    resolutionStrategy {
+        force("io.github.hfhbd.mavencentral:runtime:0.0.6")
+        dependencySubstitution {
+            substitute(module("io.github.hfhbd.mavencentral:runtime:0.0.6")).using(module("io.github.hfhbd.mavencentral:runtime:0.0.6"))
         }
     }
+}
+
+dependencies {
+    s("io.github.hfhbd.mavencentral:central-api:0.0.6")
+    s("io.github.hfhbd.mavencentral:runtime:0.0.6") {
+        endorseStrictVersions()
+        version {
+            strictly("0.0.6")
+            reject("0.0.10")
+        }
+        attributes {
+            attribute(Attribute.of("org.gradle.status", String::class.java), "release")
+        }
+    }
+}
+
+repositories {
+    mavenCentral()
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,14 +1,4 @@
-pluginManagement {
-    includeBuild("gradle/build-logic")
-    repositories {
-        mavenCentral()
-        gradlePluginPortal()
-    }
-}
-
 plugins {
-    id("myRepos")
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
     id("com.gradle.develocity") version "3.18.1"
 }
 
@@ -16,19 +6,9 @@ develocity {
     buildScan {
         termsOfUseUrl.set("https://gradle.com/terms-of-service")
         termsOfUseAgree.set("yes")
-        val isCI = providers.environmentVariable("CI").isPresent
-        publishing {
-            onlyIf { isCI }
-        }
-        tag("CI")
     }
 }
 
 rootProject.name = "mavencentral"
 
-enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
-enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
-
-include(":central-api")
 include(":runtime")
-include(":gradle-plugin")
